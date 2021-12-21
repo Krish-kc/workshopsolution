a9713a256ba0fa21fb90ea78b06011f3fcf557ad
kenish <kenkelly665@gmail.com>
2021-12-21 22:35:38 +0545

    nav bar of user

--- resources/views/userinterface/partical/navigation.blade.php
+++ resources/views/userinterface/partical/navigation.blade.php
@@ -22,10 +22,41 @@
                                 </div> -->
                       <a href="{{ route('contact') }}" class="nav-item nav-link">Contact</a>
                   </div>
-                  <div class="ml-auto">
-                      <a class="btn" href="/login">Login</a>
-                  </div>
+                  <li class="nav-item ">
+                      <div class="ml-auto ">
+                          <a class="btn" href="#" class="nav-link dropdown-toggle"
+                              id="navbarDropdownMenuLink" data-toggle="dropdown">
+                              @if (Auth::user())
+                                  {{ Auth::user()->name }}
+                              @else
+                                  Guest
+                              @endif
+                          </a>
+                          <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink">
+                              @if (Auth::user())
+
+
+                                  <a class="dropdown-item" href="#"><i class="fa fa-user" aria-hidden="true"></i>
+                                      Profile</a>
+                                  <a class="dropdown-item" href="#"><i class="fas fa-user-cog"></i>
+                                      Settings</a>
+                                  <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                 document.getElementById('logout-form').submit();"><i class="fas fa-sign-out-alt"></i>
+                                      logout</a>
+                              @else
+                                  <a class="dropdown-item" href="/login"><i class="fas fa-sign-in-alt"></i>
+                                      Login</a>
+                                  <a class="dropdown-item" href="/register"><i class="fas fa-edit"></i>
+                                      Register</a>
+
+                              @endif
+                          </div>
+                      </div>
+                  </li>
               </div>
           </nav>
       </div>
   </div>
+  <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+      @csrf
+  </form>
